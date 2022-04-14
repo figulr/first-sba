e67729ffbe0dbe91ee924f02f58ffbad4002e16f
figulr <dechallintier@gmail.com>
2022-04-14 16:39:55 +0900

    google-login 구현

---
+++ src/main/java/figulr/dechallintier/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package figulr.dechallintier.springboot.config.auth.dto;
+
+import figulr.dechallintier.springboot.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user){
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}

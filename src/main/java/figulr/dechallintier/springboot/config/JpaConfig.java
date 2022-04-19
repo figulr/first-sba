e25ea9c55252918a927f7faf6037ef958df8819e
figulr <dechallintier@gmail.com>
2022-04-19 13:22:55 +0900

    test

---
+++ src/main/java/figulr/dechallintier/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package figulr.dechallintier.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
--- src/test/java/figulr/dechallintier/springboot/web/HelloControllerTest.java
+++ src/test/java/figulr/dechallintier/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package figulr.dechallintier.springboot.web;
 
+import figulr.dechallintier.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,12 +16,16 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+                excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+                })
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void hello_returned() throws Exception{
         String hello = "hello";
         mvc.perform(get("/hello"))
@@ -26,6 +34,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDto_returned() throws Exception{
         String name="hello";
         int amount= 100;

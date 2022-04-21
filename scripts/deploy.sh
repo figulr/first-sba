ee930edbd4783d598b913d6324d29c8a42fd6a5c
figulr <dechallintier@gmail.com>
2022-04-21 13:46:17 +0900

    실제 배포 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,8 +11,6 @@ echo "> 현재 구동중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl first-sba | grep jar | awk '{print $1}')
 
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
-
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then

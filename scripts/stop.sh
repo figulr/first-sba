8d14fa7f650a939ab2e35616789ce7c4e2fdebfd
figulr <dechallintier@gmail.com>
2022-04-22 16:05:25 +0900

    무중단 stop.sh 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,12 +1,15 @@
+
+
 #!/usr/bin/env bash
 
 ABSPATH=$(readlink -f $0)
-ABSDIF=${ABSDIR}/profile.sh
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 
 IDLE_PORT=$(find_idle_port)
 
 echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
-IDLE_PID=$(lsof -it tcp:${IDLE_PORT})
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
